e58cdbbd37f1e815be8690dfe6c09e9e6bc5ad6d
Agung Trilaksono Suwarto Putra <agungtrilaksonosp@gmail.com>
2021-06-04 16:02:28 +0700

    added group user

--- asset/js/cometchat-group.js
+++ asset/js/cometchat-group.js
@@ -1,8 +1,4 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    let usersRequest = new CometChat.UsersRequestBuilder().setLimit(30).build();
-   console.log(usersRequest);
-    var appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(appRegion).build();
-    CometChat.init(appID, appSetting).then();
+function show_widget_chat(){
     CometChatWidget.init({
         "appID": appID,
         "appRegion": appRegion,
@@ -16,34 +12,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
             CometChatWidget.login({
                 uid: UID,
             }).then((loggedInUser) => {
-                var groupName = GROUP_NAME;
-                var groupType = CometChat.GROUP_TYPE.PUBLIC;
-                var password = "";
-
-                var group = new CometChat.Group(GUID, groupName, groupType, password);
-
-                CometChat.createGroup(group).then(
-                    () => {
-                        group.setIcon(group_logo_url);
-                        CometChat.updateGroup(group).then(
-                            () => {
-                                let membersList = [
-                                    new CometChat.GroupMember("admin", CometChat.GROUP_MEMBER_SCOPE.PARTICIPANT),
-                                    new CometChat.GroupMember(UID, CometChat.GROUP_MEMBER_SCOPE.PARTICIPANT),
-                                ];
-                                CometChat.addMembersToGroup(GUID, membersList, []).then(
-                                    () => {
-                                        CometChat.transferGroupOwnership(GUID, "admin").then(
-                                            () => {
-                                                CometChat.leaveGroup(GUID).then();
-                                            }
-                                        );
-                                    }
-                                );
-                            }
-                        );
-                    }
-                );
 
                 CometChatWidget.launch({
                     "widgetID": "590bc393-f078-494a-8ee8-c777b33978f3",
@@ -67,13 +35,96 @@ window.addEventListener('DOMContentLoaded', (event) => {
                         // }
                     )
                 }
-
                 
-        }, error => {
+            }, error => {
+            });
+        });
+    }, error => {
+    });
+}
+window.addEventListener('DOMContentLoaded', (event) => {
+    let usersRequest = new CometChat.UsersRequestBuilder().setLimit(30).build();
+   console.log(usersRequest);
+    var appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(appRegion).build();
+    CometChat.init(appID, appSetting).then();
+    CometChatWidget.init({
+        "appID": appID,
+        "appRegion": appRegion,
+        "authKey": authKey
+    }).then(response => {
+        CometChatWidget.login({
+            uid: "admin",
+        }).then((loggedInUser) => {
+            var groupName = GROUP_NAME;
+            var groupType = CometChat.GROUP_TYPE.PUBLIC;
+            var password = "";
+
+            var group = new CometChat.Group(GUID, groupName, groupType, password);
+
+            CometChat.createGroup(group).then(
+                () => {
+                    group.setIcon(group_logo_url);
+                    CometChat.updateGroup(group).then(
+                        () => {
+                            let membersList = [
+                                // new CometChat.GroupMember("admin", CometChat.GROUP_MEMBER_SCOPE.PARTICIPANT),
+                                new CometChat.GroupMember(UID, CometChat.GROUP_MEMBER_SCOPE.PARTICIPANT),
+                            ];
+                            CometChat.addMembersToGroup(GUID, membersList, []).then(
+                                () => {
+                                    // CometChat.transferGroupOwnership(GUID, "admin").then(
+                                    //     () => {
+                                    //         CometChat.leaveGroup(GUID).then();
+                                    //     }
+                                    // );
+                                }
+                            );
+                        }
+                    );
+                    show_widget_chat();
+                }, error => {
+                    show_widget_chat();
+                }
+            );
         });
     });
 
-}, error => {
 });
+function add_friend(){
+    CometChatWidget.init({
+        "appID": appID,
+        "appRegion": appRegion,
+        "authKey": authKey
+    }).then(response => {
+        CometChatWidget.createOrUpdateUser({
+            uid: UID,
+            name: USERNAME,
+            avatar: AVATAR
+        }).then((user) => {
+            CometChatWidget.login({
+                uid: UID,
+            }).then((loggedInUser) => {
+                var limit = 30;
+                var groupMemberRequest = new CometChat.GroupMembersRequestBuilder(GUID)
+                .setLimit(limit)
+                .build();
 
-});
+                groupMemberRequest.fetchNext().then(
+                    groupMembers => {
+                        $.each(groupMembers, function( index, value ) {
+                            if(value.status=="offline"){
+                                console.log(value.status);
+                                console.log(value.uid);
+                            } else {
+                                
+                            }
+                        });
+                    },
+                    error => {
+                        console.log("Group Member list fetching failed with exception:", error);
+                    }
+                );
+            });
+        });
+    });
+}
